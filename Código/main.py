134568b85c4d73c86825bae8fdde6ecd74151e68
Tomas Ospina Gaviria <tospinag@unal.edu.co>
2024-11-11 17:37:02 -0500

    Update main.py

---
+++ Código/main.py
@@ -0,0 +1,51 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiar_color(self, color):
+        if color in ["rojo", "verde", "amarillo", "negro", "blanco"]:
+            self.color = color
+
+class Motor:
+    def __init__(self, numero_cilindros, tipo, registro):
+        self.numero_cilindros = numero_cilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiar_registro(self, registro):
+        self.registro = registro
+
+    def asignar_tipo(self, tipo):
+        if tipo in ["gasolina", "electrico"]:
+            self.tipo = tipo
+
+class Auto:
+    cantidadCreados = 0
+    
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        Auto.cantidadCreados += 1
+
+    def cantidad_asientos(self):
+        num_asientos = 0
+        for asiento in self.asientos:
+            if asiento is not None:
+                num_asientos += 1
+        return num_asientos
+
+    def verificar_integridad(self):
+        if self.registro == self.motor.registro:
+            for asiento in self.asientos:
+                if asiento is not None:
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
